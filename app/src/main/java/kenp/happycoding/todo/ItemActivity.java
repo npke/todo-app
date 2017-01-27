eef6509592d25313b82bef67da252ab0091cac9a
Ke Nguyen Phu <kenguyen276@gmail.com>
2017-01-27 09:30:00 +0700

    Update add todo item

--- app/src/main/java/kenp/happycoding/todo/ItemActivity.java
+++ app/src/main/java/kenp/happycoding/todo/ItemActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,6 +16,7 @@ public class ItemActivity extends AppCompatActivity implements View.OnClickListe
     private EditText etTaskName;
     private RadioGroup rbTaskPriority;
     private RadioButton rbHigh, rbMedium, rbLow;
+    private DatePicker dpTaskDueDate;
 
     private Button btAdd;
 
@@ -32,6 +34,9 @@ public class ItemActivity extends AppCompatActivity implements View.OnClickListe
         rbHigh = (RadioButton) findViewById(R.id.rbHighPriority);
         rbMedium = (RadioButton) findViewById(R.id.rbMediumPiority);
         rbLow = (RadioButton) findViewById(R.id.rbLowPiority);
+
+        dpTaskDueDate = (DatePicker) findViewById(R.id.dpTaskDueDate);
+
         btAdd = (Button) findViewById(R.id.btAdd);
 
         btAdd.setOnClickListener(this);
@@ -41,12 +46,15 @@ public class ItemActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if (view.getId() == btAdd.getId()) {
-            TodoItem item = new TodoItem(0, etTaskName.getText().toString(), 1, new Date().getTime());
+            TodoItem item = new TodoItem(0, etTaskName.getText().toString(), 1, 0);
             if (rbTaskPriority.getCheckedRadioButtonId() == rbHigh.getId())
                 item.setPriority(1);
             else if (rbTaskPriority.getCheckedRadioButtonId() == rbMedium.getId()) {
                 item.setPriority(2);
             } else item.setPriority(3);
+
+            Date dueDate = new Date(dpTaskDueDate.getYear() - 1900, dpTaskDueDate.getMonth(), dpTaskDueDate.getDayOfMonth());
+            item.setDueDate(dueDate.getTime());
 
             database.addTodoItem(item);
             finish();
